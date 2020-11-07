b22b4f947fdf32d5f5dbb90c15d060e7882697f7
nichotelo <nicolaschristianto@mail.ugm.ac.id>
2020-11-08 00:53:00 +0700

    urdf_ws basic

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/nichotelo/ros/urdf_ws/src
+# Build directory: /home/nichotelo/ros/urdf_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("r2d2")
+subdirs("robot_setup_tf")
+subdirs("learning_tf")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/nichotelo/ros/urdf_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/nichotelo/ros/urdf_ws/devel;/home/nichotelo/ros/robotmodel_ws/devel;/home/nichotelo/ros/wanderbot_ws/devel;/home/nichotelo/ros/2wd_robot/devel;/home/nichotelo/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/nichotelo/ros/urdf_ws/devel;/home/nichotelo/ros/robotmodel_ws/devel;/home/nichotelo/ros/wanderbot_ws/devel;/home/nichotelo/ros/2wd_robot/devel;/home/nichotelo/catkin_ws/devel;/opt/ros/noetic' != '' else []
